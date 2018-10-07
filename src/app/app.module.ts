bcdd2f5a36b14b55109770714eda348d31e36d86
Paul Sons <psons@psons@comcast.net>
2018-10-07 13:02:48 -0500

    page 203

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+
+import {
+  RouterModule,
+  Routes
+} from '@angular/router';
+import {LocationStrategy,
+HashLocationStrategy,
+APP_BASE_HREF
+} from '@angular/common';
 
 import { AppComponent } from './app.component';
+// Album and Aritst components need to be generated
 
+// The stuff below is generated stuff, not shown in the book solution as of p205.
 @NgModule({
   declarations: [
     AppComponent
