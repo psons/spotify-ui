b4b8fa5b3f62c970c1646af92a4f57a861e739e6
Paul Sons <psons@psons@comcast.net>
2018-10-07 13:28:29 -0500

    page 204 first version src/app/spotify.service.ts

---
+++ src/app/spotify.service.ts
@@ -0,0 +1,18 @@
+
+// IDE markes this class as Deprecated.
+import {Http} from '@angular/http';
+
+class SpotifyService {
+  constructor(public http: Http) {
+
+  }
+
+  searchTrack(query: string) {
+    let params: string = [
+      `q=${query}`,
+      `type=track`
+    ].join(`&`);
+    let queryURL: string = `https://api.spotify.com/v1/search?${params}`;
+    return this.http.request(queryURL).map(res => res.json());
+  }
+}
